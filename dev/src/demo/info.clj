daefe141e113c6fdd10466858e0209f720aadfc6
awb99 <awb99@gmail.com>
2025-03-14 12:19:51 -0500

    market-info

---
+++ dev/src/demo/info.clj
@@ -0,0 +1,27 @@
+(ns demo.info
+  (:require
+   [tick.core :as t]
+   [quanta.calendar.calendar-info :refer [market-info gather-calendar]]
+   ))
+
+
+(def dt (t/instant))
+
+
+
+(market-info :crypto dt)
+
+;; => {:market :crypto, 
+;;     :open? true, 
+;;     :business true, :
+;;      as-of-dt #time/zoned-date-time "2024-10-11T21:10:02.760020057Z[UTC]"}
+
+
+(gather-calendar [:crypto :h] dt)
+;{:calendar [:crypto :h],
+; :business true,
+; :open true,
+; :zone "UTC",
+; :calendar-time #time/date-time "2025-03-14T17:17:18.293494791",
+; :current #time/instant "2025-03-14T17:00:00Z",
+; :next #time/instant "2025-03-14T18:00:00Z"}
