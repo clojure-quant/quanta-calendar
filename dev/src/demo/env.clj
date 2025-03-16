ddb28c86edd572e7e04fc12501b7c130f9426e06
awb99 <awb99@gmail.com>
2025-03-16 13:54:58 -0500

    scheduler with initialized values

--- dev/src/demo/env.clj
+++ dev/src/demo/env.clj
@@ -49,6 +49,31 @@ crypto
 
 (stop2!)
 
+;; continuous
+
+(defn multiple-dt [i1 i2]
+  {:m (-> i1 i/current :close)
+   :m5 (-> i2 i/current :close)})
+
+(def multiple
+  (m/latest multiple-dt (get-calendar env {:calendar [:crypto :m]})
+            (get-calendar env {:calendar [:crypto :m5]})))
+
+(def stop3!
+  ((m/reduce (fn [s v]
+               (println "dt multiple: " v)
+               (spit "bongo.txt" (str "\ndt: " v) :append true)) nil multiple)
+   #(prn "success " %)
+   #(prn "crash " %)))
+
+(set-dt env (t/instant))
+
+(set-dt env (t/instant "2025-02-01T00:13:00Z"))
+(set-dt env (t/instant "2025-01-01T00:13:04Z"))
+
+(set-dt env nil)
+
+(stop3!)
 
 
 
--- src/quanta/calendar/env/scheduler.clj
+++ src/quanta/calendar/env/scheduler.clj
@@ -4,95 +4,100 @@
    [missionary.core :as m]
    [quanta.calendar.interval :as i]
    [quanta.calendar.db.calendars :refer [get-calendar-list]]
-   [quanta.calendar.db.interval :refer [next-upcoming-close last-finished-close all-intervals]])
+   [quanta.calendar.db.interval :refer [last-finished-close all-intervals]])
   (:import [missionary Cancelled]))
 
-(defn scheduler-f
+(defn scheduler
   "returns a missionary flow
    fires current and all upcoming timestamps for a calendar"
-  [calendar current]
-  (m/ap
-   (println "live-calendar " calendar " starting with: " (i/current current))
-    ;(m/amb current)
-   (try
-     (loop [c current]
-       (let [current-dt-long (-> (t/instant) t/long)
-             next-dt-instant  (-> c i/current :close)
-             next-dt-long  (t/long next-dt-instant)
-             diff-ms (* 1000 (- next-dt-long current-dt-long))]
-         (when (> diff-ms 0)
-           (println "live-calendar " calendar " sleeping for ms: " diff-ms " until: " next-dt-instant)
-           (m/? (m/sleep diff-ms c))))
-       (m/amb
-        c
-        (recur (i/move-next c))))
-     (catch Cancelled cancel
-       (println "live-calendar " calendar " stopped.")))))
-
-(defn scheduler [calendar]
-  (let [current (last-finished-close calendar (t/instant))]
-    (->> (scheduler-f calendar current)
-         (m/reductions (fn [r v] v) (i/current current))
-         (m/relieve {})
+  [calendar]
+  (let [current (last-finished-close calendar (t/instant))
+        f (m/ap
+           (println "live-calendar " calendar " starting with: " (i/current current))
+           (try
+             (loop [c (i/move-next current)]
+               (let [current-dt-long (-> (t/instant) t/long)
+                     next-dt-instant  (-> c i/current :close)
+                     next-dt-long  (t/long next-dt-instant)
+                     diff-ms (* 1000 (- next-dt-long current-dt-long))]
+                 (when (> diff-ms 0)
+                   (println "live-calendar " calendar " sleeping for ms: " diff-ms " until: " next-dt-instant)
+                   (m/? (m/sleep diff-ms c))))
+               (m/amb
+                c
+                (recur (i/move-next c))))
+             (catch Cancelled _cancel
+               (println "live-calendar " calendar " stopped.")
+               (m/amb))))]
+    (->> f
+         (m/reductions {} current)
+         ;(m/reductions (fn [r v] v) (i/current current))
+         ;(m/relieve {})
          (m/signal))))
 
-(defn all-calendars []
+(defn delayed-scheduler [calendar delay-ms]
+  (let [current (last-finished-close calendar (t/instant))
+        f (scheduler calendar)
+        delayed-f (m/ap
+                   (let [cur (m/?> f)]
+                     (m/? (m/sleep delay-ms cur))))]
+    (->> delayed-f
+         (m/reductions {} current)
+             ;(m/reductions (fn [r v] v) (i/current current))
+             ;(m/relieve {})
+         (m/signal))))
+
+(defn create-calendar-flow-dict []
   (->> (for [c (get-calendar-list)
              i all-intervals]
          (let [cal [c i]]
            [cal (scheduler cal)]))
        (into {})))
 
-(def calendar-dict (all-calendars))
+(def calendar-dict (create-calendar-flow-dict))
+
+calendar-dict
 
 (defn get-calendar-flow
   ([calendar]
    (get calendar-dict calendar))
   ([calendar delay-ms]
-   (m/stream
-    (m/ap
-     (let [cal-f (get-calendar-flow calendar)
-           cur (m/?> cal-f)]
-       (m/? (m/sleep delay-ms))
-       cur)))))
+   (delayed-scheduler calendar delay-ms)))
 
-(defn get-calendar-flow-close-date
-  "returns a flow of instant for the calendar.
-   depending on the env (and set-dt) the calendar is
-   either live or historic."
-  ([calendar]
-   (->> (get-calendar-flow calendar)
-        (m/eduction
-         (map i/current)
-         (map :close))
-        (m/stream)))
-  ([calendar delay-ms]
-   (->> (get-calendar-flow calendar delay-ms)
-        (m/eduction
-         (map i/current)
-         (map :close))
-        (m/stream))))
+(defn as-close-date-flow [cal-flow]
+  (->> cal-flow
+       (m/eduction
+        (map i/current)
+        (map :close))
+       (m/signal)))
 
 (comment
 
-  (m/? (->> (scheduler [:us :d])
-            (m/eduction (take 1) (map i/current))
+  (m/? (->> (get-calendar-flow [:forex :m])
+            (m/eduction (take 1)
+                        (map i/current)
+                        (map :close))
+            (m/reduce conj)))
+
+  (m/? (->> (get-calendar-flow [:forex :m] 3000)
+            (m/eduction (take 1)
+                        (map i/current)
+                        (map :close))
             (m/reduce conj)))
 
   (do (println "start time: " (t/instant))
       (println "Result: "
                (m/? (->> ;(scheduler [:forex :m])
-                     (scheduler [:crypto :m])
-                     (m/eduction (take 2) (map i/current))
+                     (get-calendar-flow [:crypto :m])
+                     (m/eduction (take 2)
+                                 (map i/current)
+                                 (map :close))
                      (m/reduce conj))))
       (println "end time: " (t/instant)))
 
-  (m/? (->> (scheduler [:forex :m])
+  (m/? (->> (scheduler [:crypto :m])
             (m/eduction (take 2))
             (m/reduce conj)))
 
-  (get-calendar-flow [:forex :m])
-  (get-calendar-flow [:forex :m333])
-
- ;
+;
   )
